e52ed8374bfa9650b1bca9e302d210e02fc8fb08
Vadym Liesin <scaledicy@gmail.com>
2021-10-30 23:08:21 +0300

    Create a feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 //Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
